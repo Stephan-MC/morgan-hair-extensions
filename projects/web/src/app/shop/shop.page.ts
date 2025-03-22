b41c7dc92bbf49edf738419add7e053377bbef16
Minister <mundestephane13@gmail.com>
2025-03-22 17:40:10 +0100

    little optimizations on shop page

--- projects/web/src/app/shop/shop.page.ts
+++ projects/web/src/app/shop/shop.page.ts
@@ -1,12 +1,19 @@
-import { Component, effect, inject, input, signal } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import {
+  Component,
+  effect,
+  inject,
+  input,
+  linkedSignal,
+  signal,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { PaginationComponent } from '../common/components/pagination/pagination.component';
 import { ProductCardSkeletonComponent } from '../common/components/product-card-skeleton/product-card-skeleton.component';
-import { ProductCardComponent } from '../common/components/product-card/product-card.component';
 import { ShopFilterComponent } from '../common/components/shop-filter/shop-filter.component';
 import { WigCardComponent } from '../common/components/wig-card/wig-card.component';
 import { WigService } from '../common/services/wig.service';
 import { CartStore } from '../common/stores/cart.store';
-import { PaginationComponent } from '../common/components/pagination/pagination.component';
 import { WigFilter } from '../common/types/wig';
 
 @Component({
@@ -25,19 +32,26 @@ import { WigFilter } from '../common/types/wig';
   },
 })
 export class ShopPage {
+  readonly viewPortScroller = inject(ViewportScroller);
   private _wigService = inject(WigService);
   wigs = this._wigService.wigsResource;
   cart = inject(CartStore);
   filterOpen = signal(false);
 
-  page = input<number>();
+  _page = input<number>(undefined, { alias: 'page' });
+  page = linkedSignal(() => this._page());
 
   constructor() {
     effect(() => {
       if (this.page()) {
+        this.viewPortScroller.scrollToPosition([0, 0]);
         this.patchFilters({ page: this.page() });
       }
     });
+  }
+
+  ngAfterViewInit() {
+    console.log('what the fuck');
   }
 
   hasFilters() {
