f00cdd5ae164a63960cc97c8e54461e8ce6e2fd3
Minister <mundestephane13@gmail.com>
2025-03-29 00:28:57 +0100

    updated shared exports

--- projects/shared/src/public-api.ts
+++ projects/shared/src/public-api.ts
@@ -2,5 +2,7 @@
  * Public API Surface of shared
  */
 
-export * from './lib/shared.service';
-export * from './lib/shared.component';
+export * from './lib/services';
+export * from './lib/components';
+export * from './lib/types';
+export * from './lib/helpers';
