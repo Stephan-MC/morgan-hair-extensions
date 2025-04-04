f45e802ec92c0d0cd8016741cd9deb247850d071
Minister <mundestephane13@gmail.com>
2025-04-04 08:35:31 +0100

    Added disclaimer to cart page

--- projects/web/src/app/cart/cart.page.ts
+++ projects/web/src/app/cart/cart.page.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { CartStore } from '../common/stores/cart.store';
 import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { ImageComponent } from 'shared';
 
 @Component({
   selector: 'web-cart',
-  imports: [CurrencyPipe, NgOptimizedImage, RouterLink],
+  imports: [CurrencyPipe, RouterLink, ImageComponent],
   templateUrl: './cart.page.html',
   styleUrl: './cart.page.css',
 })
