5e1305f75ef558a6f0e6a04d459b4848d5990202
Minister <mundestephane13@gmail.com>
2025-04-14 20:05:15 +0100

    Added footer component

--- projects/web/src/app/app.component.ts
+++ projects/web/src/app/app.component.ts
@@ -4,18 +4,29 @@ import { NavbarComponent } from './common/components/navbar/navbar.component';
 import { CartComponent } from './common/components/cart/cart.component';
 import { CartStore } from './common/stores/cart.store';
 import { Meta } from '@angular/platform-browser';
+import { ClientStore } from 'shared';
+import { FooterComponent } from './common/components/footer/footer.component';
 
 @Component({
   selector: 'web-root',
-  imports: [RouterOutlet, NavbarComponent, CartComponent],
+  imports: [RouterOutlet, NavbarComponent, CartComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'web';
   cart = inject(CartStore);
+  client = inject(ClientStore);
 
   constructor(meta: Meta) {
     meta.addTag({ name: 'author', content: 'Juliet Morgan' });
+  }
+
+  ngAfterViewInit() {
+    this.client.login({
+      email: 'client1@morganhairextensions.localhost',
+      password: 'password',
+      remember: true,
+    });
   }
 }
