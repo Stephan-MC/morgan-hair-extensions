e7d1daf2426242e54756cbab74e34fcbfac34505
Minister <mundestephane13@gmail.com>
2025-05-17 13:53:45 +0100

    Complete the footer

--- projects/web/src/app/common/components/footer/footer.component.ts
+++ projects/web/src/app/common/components/footer/footer.component.ts
@@ -1,14 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TextInputComponent } from "shared";
 
 @Component({
-  selector: 'web-footer',
-  imports: [],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css',
-  host: {
-    'class': 'space-y-12'
-  }
+	selector: "web-footer",
+	imports: [TextInputComponent],
+	templateUrl: "./footer.component.html",
+	styleUrl: "./footer.component.css",
+	host: {
+		class: "space-y-12",
+	},
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
