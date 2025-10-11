fa2775c7af038ea0119ed0df71febcfbdc92ceb7
Minister <mundestephane13@gmail.com>
2025-10-11 02:18:56 +0100

    Revamped server routes

--- projects/web/src/app/app.routes.server.ts
+++ projects/web/src/app/app.routes.server.ts
@@ -3,6 +3,10 @@ import { RenderMode, ServerRoute } from "@angular/ssr";
 export const serverRoutes: ServerRoute[] = [
 	{
 		path: "**",
+		renderMode: RenderMode.Prerender,
+	},
+	{
+		path: "wig/:slug",
 		renderMode: RenderMode.Server,
 	},
 ];
